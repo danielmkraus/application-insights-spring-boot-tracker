36d360a08d6bc6326ccea78bd36d98517ae18154
Daniel Kraus <danielmkraus@gmail.com>
2024-02-19 01:52:22 +0100

    [AIST-6] Sonar issue

--- src/test/java/org/danielmkraus/applicationinsights/aop/InterceptorDisabledIntegrationTest.java
+++ src/test/java/org/danielmkraus/applicationinsights/aop/InterceptorDisabledIntegrationTest.java
@@ -29,7 +29,7 @@ import static org.mockito.Mockito.verifyNoInteractions;
 @DirtiesContext
 @TestPropertySource("classpath:application.yml")
 @EnableApplicationInsightsDependencyTracer
-public class InterceptorDisabledIntegrationTest {
+class InterceptorDisabledIntegrationTest {
 
     @Autowired
     SampleController controller;
